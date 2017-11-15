fd835edbe9b3e9117f70d054afb6c1aeb19ac32a
Adrià Bonet Vidal <Adria331@users.noreply.github.com>
2017-11-15 17:20:07 +0100

    Add files via upload

--- ServerImp.java
+++ ServerImp.java
@@ -1,1 +1,56 @@
-print "oda"
+
+public class ServerImp extends UnicastRemoteObject implements ServerInterface{
+	private String filePath;
+	private String ipAdd;
+	private int port;
+
+
+	public ServerImp() throws RemoteException{
+		super();
+	}
+
+
+	public void registerClient(User user) throws RemoteException{
+
+	}
+
+
+	public void discardClient(User user) throws RemoteException{
+
+	}
+
+
+	public List<String> getContent(String description) throws RemoteException{
+
+	}
+
+
+	public void downloadContent(String title) throws RemoteException{
+
+	} 
+
+
+	public void notifyAllClients() throws RemoteException{
+
+	}
+
+
+	public getContentAdvanced() throws RemoteException{
+
+	}
+
+
+	public void downloadAdvanced() throws RemoteException{
+
+	} 
+
+
+	public void modifyContentTitle(String oldTitle, String newTitle,  User user) throws RemoteException{
+
+	}
+
+
+	public void deleteContent(String title, User user) throws RemoteException{
+
+	}
+}
